8fcf4b9cec3800f2515c4d38d54cf0f693e47d87
Layla090 <skoka2028@visi.org>
2025-12-19 19:29:47 +0000

    added countdown timer

---
+++ script.js
@@ -0,0 +1,23 @@
+<script>
+
+    var countDownDate = new Date("Dec 25, 2025 15:37:25"),getTime();
+
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = CountDown - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("CountDown").innerHTML = days + "d " + hours + "h" + minutes + "m" + seconds + "s";
+        
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("CountDown").innerHTML = "MERRY CHRISTMAS";
+        }
+    }, 1000);
+
+
+    </script>
